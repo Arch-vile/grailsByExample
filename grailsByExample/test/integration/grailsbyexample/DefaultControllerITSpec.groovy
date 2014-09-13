2d24966afd635f282d158ea62a00de4560975338
ra1p3 <raipe.spamfilter@gmail.com>
2014-09-13 12:15:02 +0300

    Integration test example

---
+++ grailsByExample/test/integration/grailsbyexample/DefaultControllerITSpec.groovy
@@ -0,0 +1,34 @@
+package grailsbyexample
+
+
+
+import spock.lang.*
+
+/**
+ *
+ */
+class DefaultControllerITSpec extends Specification {
+
+    DefaultController controller
+    EchoService echoService
+
+    def setup() {
+        controller = new DefaultController()
+        echoService = Mock(EchoService)
+        controller.echoService = echoService
+    }
+
+    def cleanup() {
+    }
+
+    void "Verify service is called with expected arguments"() {
+        when: 'Controller method is called'
+        controller.echoMe("Something to echo")
+
+        then: 'Service called with the same argument'
+        1 * echoService.echo({ text ->
+            assert text == "Something to echo"
+            true
+        })
+    }
+}
